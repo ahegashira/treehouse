b02cd8f32e14f18bc61a61d517db850d5d2227b5
Hiro Egashira <ahegashira@gmail.com>
2019-12-18 15:00:18 -0800

    Create PezDispenser.java

---
+++ java-objects/PezDispenser.java
@@ -0,0 +1,11 @@
+class PezDispenser {
+    final private String characterName;
+
+    public PezDispenser(String characterName) {
+        this.characterName = characterName;
+    }
+
+    public String getCharacterName() {
+        return characterName;
+    }   
+}
