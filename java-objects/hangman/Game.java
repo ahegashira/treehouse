66c6f5928e2fd5a5be7ae360894ff3bd6d8909cf
Hiro Egashira <ahegashira@gmail.com>
2019-12-20 14:02:34 -0800

    add getRemainingTries() method
    to calculate remaining guesses

--- java-objects/hangman/Game.java
+++ java-objects/hangman/Game.java
@@ -1,15 +1,17 @@
 class Game {
+    public static final int MAX_MISSES = 7;
     private String answer;
     private String hits;
     private String misses;
 
-
+    // Track correct and incorrect guesses
     public Game(String answer) {
         this.answer = answer;
         hits = "";
         misses = "";
     }
 
+    // Take user input and apply to correct or incorrect guesses
     public boolean applyGuess(char letter) {
         boolean isHit = answer.indexOf(letter) != -1;
         if (isHit) {
@@ -20,6 +22,12 @@ class Game {
         return isHit;
     }
 
+    // Calculate number of guesses remaining
+    public int getRemainingTries() {
+        return MAX_MISSES - misses.length();
+    }
+
+    // Track progress of game to be displayed by displayProgress()
     public String getCurrentProgress() {
         String progress = "";
         for (char letter : answer.toCharArray()) {
